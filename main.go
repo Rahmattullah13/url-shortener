af7c2f4871b9c001307fbd4d354b3352d102d38d
Rahmattullah <rfkrhmttllh@gmail.com>
2023-03-12 11:16:42 +0700

    intial commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Rahmattullah13/url-shortener/handler"
+	"github.com/Rahmattullah13/url-shortener/storage"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Welcome to the URL Shortener API",
+		})
+	})
+
+	r.POST("/create-short-url", func(ctx *gin.Context) {
+		handler.CreateShortUrl(ctx)
+	})
+
+	r.GET("/:shortUrl", func(ctx *gin.Context) {
+		handler.HandleShortUrlRedirect(ctx)
+	})
+
+	// Note that store initialization happens here
+	storage.InitializeStore()
+
+	err := r.Run(":9808")
+	if err != nil {
+		panic(fmt.Sprintf("Failed to start the web server - Error: %v", err))
+	}
+}
